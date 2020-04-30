b4d4ad736dc982efcc2d7b4e3a04c002a9c9afeb
nico91d <64389649+nico91d@users.noreply.github.com>
2020-04-30 17:40:31 -0300

    Update contacto.php

--- contacto.php
+++ contacto.php
@@ -4,7 +4,7 @@
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>Contacto | </title>
+  <title>Contacto | Fundación Ark Ho </title>
   <meta content="" name="descriptison">
   <meta content="" name="keywords">
 
