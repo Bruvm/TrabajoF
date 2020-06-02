8f3a2466149c81888b62d191b67ed16e4faa898f
Rolanader20 <64048818+Rolanader20@users.noreply.github.com>
2020-06-02 18:39:55 -0300

    Update index.php

--- index.php
+++ index.php
@@ -265,7 +265,7 @@
   </main><!-- End #main -->
 
   <!-- ======= Footer ======= -->
-
+  <?php include ('footer.php'); ?>
   
   <a href="#" class="back-to-top"><i class="icofont-simple-up"></i></a>
 
