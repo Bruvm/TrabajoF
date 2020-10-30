8ff6ef30e4cd49b97f059d1c4ddf1e0c5944be2c
Melina Bruvera <melibruvera@hotmail.com>
2020-10-30 17:44:12 -0300

    Update menu.php

--- menu.php
+++ menu.php
@@ -1,73 +1,27 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="utf-8">
-  <meta content="width=device-width, initial-scale=1.0" name="viewport">
-
-  <title>Fundación Ark Ho</title>
-  <meta content="" name="descriptison">
-  <meta content="" name="keywords">
-
-  <!--link de los social media en la parte de abajo de la pagina-->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
-
-  <!-- Favicons -->
-  <link href="assets/img/favicon.png" rel="icon">
-  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
-
-  <!-- Google Fonts -->
-  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
-
-  <!-- Vendor CSS Files -->
-  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <link href="assets/vendor/icofont/icofont.min.css" rel="stylesheet">
-  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-  <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
-  <link href="assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
-  <link href="assets/vendor/venobox/venobox.css" rel="stylesheet">
-
-  <!-- Template Main CSS File -->
-  <link href="assets/css/style.css" rel="stylesheet">
-</head>
-
-<body>
-  <?php include ('conexion.php'); ?>
   <header id="header">
     <div class="container d-flex">
 
       <div class="logo mr-auto">
-        <a href="index.html"><img src="assets/img/menu/logo.png" alt="" class="img-fluid"></a>
+        <a href="index.php"><img src="assets/img/menu/logo.png" alt="" class="img-fluid"></a>
       </div>
 
       <nav class="nav-menu d-none d-lg-block">
         <ul>
-          <li class="active"><a href="index.html">Home</a></li>
-          <li><a href="#">Nosotros</a></li>
-          <li><a href="#">Inclusión Educativa</a></li>
-          <li><a href="#">Preguntas Frecuentes</a></li>
-          <li><a href="#">Información Destacada</a></li>
-          <li><a href="#">Contacto</a></li>
+          <li id="home"><a href="index.php">Home</a></li>
+          <li class="drop-down" id="institucional"><a href="#">Institucional</a>
+            <ul>
+              <li><a href="nosotros.php">Nosotros</a></li>
+              <li><a href="team.html">Estatuto</a></li>
+              <li><a href="team.html">Leyes de discapacidad </a></li>
+            </ul>
+          </li>
+          <li id="integracion"><a href="integracion.php">Integración Educativa</a></li>
+          <li id="preguntas"><a href="preguntas.php">Preguntas Frecuentes</a></li>
+          <li id="destacadas"><a href="ndestacadas.php">Información Destacada</a></li>
+          <li id="contacto"><a href="contacto.php">Contacto</a></li>
 
         </ul>
       </nav><!-- .nav-menu -->
 
     </div>
   </header><!-- End Header -->
-  
-  <!-- Vendor JS Files -->
-  <script src="assets/vendor/jquery/jquery.min.js"></script>
-  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="assets/vendor/jquery.easing/jquery.easing.min.js"></script>
-  <script src="assets/vendor/jquery-sticky/jquery.sticky.js"></script>
-  <script src="assets/vendor/owl.carousel/owl.carousel.min.js"></script>
-  <script src="assets/vendor/waypoints/jquery.waypoints.min.js"></script>
-  <script src="assets/vendor/counterup/counterup.min.js"></script>
-  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
-  <script src="assets/vendor/venobox/venobox.min.js"></script>
-
-  <!-- Template Main JS File -->
-  <script src="assets/js/main.js"></script>
-
-</body>
-</html>
