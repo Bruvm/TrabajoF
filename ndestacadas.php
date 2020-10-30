fbc2e71de74d40a54ba1cc2d0a48fc30eb9826cd
Melina Bruvera <melibruvera@hotmail.com>
2020-10-30 17:44:14 -0300

    Update ndestacadas.php

--- ndestacadas.php
+++ ndestacadas.php
@@ -1,96 +1,30 @@
 <!DOCTYPE html>
 <html lang="en">
-
-<head>
-  <meta charset="utf-8">
-  <meta content="width=device-width, initial-scale=1.0" name="viewport">
-
-  <title>Información Destacadas</title>
-  <meta content="" name="descriptison">
-  <meta content="" name="keywords">
-
-
-  <!--link de los social media en la parte de abajo de la pagina-->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
-
-
-  <!-- Favicons -->
-  <link href="assets/img/favicon.png" rel="icon">
-  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
-
-  <!-- Google Fonts -->
-  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
-
-  <!-- Vendor CSS Files -->
-  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <link href="assets/vendor/icofont/icofont.min.css" rel="stylesheet">
-  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-  <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
-  <link href="assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
-  <link href="assets/vendor/venobox/venobox.css" rel="stylesheet">
-
-  <!-- Template Main CSS File -->
-  <link href="assets/css/style.css" rel="stylesheet">
-
-
-
-</head>
+<?php include ('head.php'); ?>
 
 <body>
 <?php include ('conexion.php'); ?>
-  <header id="header">
-    <div class="container d-flex">
-
-      <div class="logo mr-auto">
-        <a href="index.php"><img src="assets/img/menu/logo.png" alt="" class="img-fluid"></a>
-      </div>
-
-      <nav class="nav-menu d-none d-lg-block">
-        <ul>
-          <li><a href="index.php">Home</a></li>
-          <li class="drop-down"><a href="#">Institucional</a>
-            <ul>
-              <li><a href="nosotros.php">Nosotros</a></li>
-              <li><a href="team.html">Estatuto</a></li>
-              <li><a href="team.html">Leyes de discapacidad </a></li>
-            </ul>
-          </li>
-          <li><a href="integracion.php">Integración Educativa</a></li>
-          <li><a href="preguntas.php">Preguntas Frecuentes</a></li>
-          <li class="active"><a href="#">Información Destacada</a></li>
-          <li><a href="contacto.php">Contacto</a></li>
-
-        </ul>
-
-      </nav><!-- .nav-menu -->
-
-    </div>
-  </header><!-- End Header -->
+<?php include ('menu.php'); ?>
 
   <main id="main">
 
   </header><!-- End Header -->
-
       <section id="breadcrumbs" class="breadcrumbs">
       <div class="container">
-
         <ol>
           <li><a href="index.php">ARK HO</a></li>
           <li>Información destacada</li>
         </ol>
         <h2>Información destacada</h2>
-
       </div>
     </section>
 
 
-    
-
-        <!-- ======= Blog Section ======= -->
+    <!-- ======= Blog Section ======= -->
     <section id="blog" class="blog ">
       <div class="container">
         <div class="section-title">
-          <h2>Noticia destacada</h2>
+          <h2 style="text-transform: uppercase;">Noticia destacada</h2>
         </div>
 
         <div class="row justify-content-center">
@@ -129,14 +63,14 @@
       </div>
     </section><!-- End Blog Section -->
 
-    <section id="about" class="about">
+    <section id="about" class="about" style="background: #FFF7F7;">
         <div class="container">
             <div class="section-title">
-              <h2>Últimas Noticias</h2>
+              <h2 style="text-transform: uppercase;">Últimas Noticias</h2>
             </div>
 
 
-        <div class="row">
+        <div class="row pt-3 pb-3" style="background: #fff; box-shadow: 0 4px 16px rgba(0, 0, 0, 0.1)">
           <div class="col-lg-6">
             <img src="assets/img/ndestacadas/nd1.jpg" class="img-fluid" alt="">
           </div>
@@ -161,7 +95,7 @@
         </div>     <br><br> 
 
 
-        <div class="row">      
+        <div class="row pt-3 pb-3" style="background: #fff; box-shadow: 0 4px 16px rgba(0, 0, 0, 0.1)">     
             <div class="col-lg-6 pt-4 pt-lg-0 content order-2 order-lg-1">
               <h3>Igualdad de derechos</h3>
               <p class="font-italic">
@@ -208,19 +142,15 @@
 
   <a href="#" class="back-to-top"><i class="icofont-simple-up"></i></a>
 
-  <!-- Vendor JS Files -->
-  <script src="assets/vendor/jquery/jquery.min.js"></script>
-  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="assets/vendor/jquery.easing/jquery.easing.min.js"></script>
-  <script src="assets/vendor/jquery-sticky/jquery.sticky.js"></script>
-  <script src="assets/vendor/owl.carousel/owl.carousel.min.js"></script>
-  <script src="assets/vendor/waypoints/jquery.waypoints.min.js"></script>
-  <script src="assets/vendor/counterup/counterup.min.js"></script>
-  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
-  <script src="assets/vendor/venobox/venobox.min.js"></script>
+  <?php include ('script.php'); ?>
 
-  <!-- Template Main JS File -->
-  <script src="assets/js/main.js"></script>
+  <script type="text/javascript">
+    $(document).ready(function() {
+      $('#destacadas').addClass('active');
+    });
+    
+  </script>
+</body>
 
 </body>
 
