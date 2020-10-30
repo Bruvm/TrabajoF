848cb7fad31ae2a47cd4960305e5949310d8f593
meliBruve <melibruve@gmail.com>
2020-10-29 21:52:05 -0300

    Create ndestacada-uno.php

---
+++ noticias/ndestacada-uno.php
@@ -0,0 +1,155 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Información Importante</title>
+  <meta content="" name="descriptison">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/icofont/icofont.min.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+  <link href="assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="assets/vendor/venobox/venobox.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+</head>
+
+<body>
+<?php include ('conexion.php'); ?>
+  <header id="header">
+    <div class="container d-flex">
+
+      <div class="logo mr-auto">
+        <a href="index.php"><img src="assets/img/menu/logo.png" alt="" class="img-fluid"></a>
+      </div>
+
+      <nav class="nav-menu d-none d-lg-block">
+        <ul>
+          <li><a href="index.php">Home</a></li>
+          <li><a href="nosotros.php">Nosotros</a></li>
+          <li><a href="inclusion.php">Inclusión escolar</a></li>
+          <li><a href="preguntas.php">Preguntas frecuentes</a></li>
+          <li  class="active"><a href="ndestacadas.php">Información destacada</a></li> 
+          <li><a href="contacto.php">Contacto</a></li>
+
+        </ul>
+      </nav><!-- .nav-menu -->
+
+    </div>
+  </header><!-- End Header -->
+  <main id="main">
+
+    <!-- ======= Breadcrumbs ======= -->
+    <section id="breadcrumbs" class="breadcrumbs">
+      <div class="container">
+
+        <ol>
+          <li><a href="index.php">ARK HO</a></li>
+          <li><a href="ndestacadas.php">Noticias Destacadas</a></li>
+        </ol>
+        <h2>¡Pasamos la auditoria!</h2>
+
+      </div>
+    </section><!-- End Breadcrumbs -->
+
+    <!-- ======= Blog Section ======= -->
+    <section id="blog" class="blog">
+      <div class="container">
+
+        <div class="row justify-content-center">
+
+          <div class="col-lg-8 entries">
+
+            <article class="entry entry-single ">
+
+              <div class="entry-img">
+                <img src="assets/img/ndestacadas/ndd.png" alt="" class="img-fluid">
+              </div>
+
+              <h2 class="entry-title">
+                <a>Una vez más, hemos cumplido con el protocolo de la súper intendencia de salud.</a>
+              </h2>
+
+
+              <div class="entry-content">
+
+                <p>
+                  Estamos orgullosos y felices en contarles que hemos pasado la nueva auditoría por parte de la super intendencia de salud. Las cual nos destaca como un "Centro categorizado". </p>
+                
+                  <h3>
+                    ¿Qué nos solicitan para obtener esta categorización?
+                  </h3>
+                  <p>
+                   Para ello debemos tener: habilitación de municipal, de salud de consultorio y de bomberos. Asi como también, contar con una infraestructura que sea accesible para las personas con discapacidad, tener una manera de pensar las historias clínicas, devoluciones diarias de todo lo que hacen los pacientes y tener cierta calidad institucional. 
+                 </p>
+                   <p>
+                   Debemos contar con personeria jurídica,  y pasar estas auditorias anuales por parte del ministerio de salud, del área de ruge presa que da la habilitación de consultorio, del área de la superintendencia que es la junta categorización y auditación.
+
+                </p>
+
+
+
+                <p>
+                  ¡<strong>Gracias</strong> por todo el esfuerzo a los profesionales!
+                </p>
+
+               <br>
+              </div>
+
+              <nav class="nav-menu d-none d-lg-block">
+        <ul>
+        
+          <li  class="active"><a href="ndestacadas.php">Entradas recientes >></a></li>
+          
+
+        </ul>
+      </nav><!-- .nav-menu -->
+      <br>
+
+
+
+            </article><!-- End blog entry -->
+          </div>
+        </div>
+      </div>
+    </section>
+  </main>
+
+
+  <?php include ('footer.php'); ?>
+
+  <a href="#" class="back-to-top"><i class="icofont-simple-up"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/jquery/jquery.min.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/jquery.easing/jquery.easing.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+  <script src="assets/vendor/jquery-sticky/jquery.sticky.js"></script>
+  <script src="assets/vendor/owl.carousel/owl.carousel.min.js"></script>
+  <script src="assets/vendor/waypoints/jquery.waypoints.min.js"></script>
+  <script src="assets/vendor/counterup/counterup.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/venobox/venobox.min.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
