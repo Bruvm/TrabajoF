3a6a3c03b93ac2651b0d58e79d5b3e41808b54f8
meliBruve <melibruve@gmail.com>
2020-10-29 21:52:09 -0300

    Create ndestacada-dos.php

---
+++ noticias/ndestacada-dos.php
@@ -0,0 +1,159 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Información Importante</title>
+  <meta content="" name="descriptison">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/icofont/icofont.min.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+  <link href="assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="assets/vendor/venobox/venobox.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+</head>
+
+<body>
+<?php include ('conexion.php'); ?>
+  <header id="header">
+    <div class="container d-flex">
+
+      <div class="logo mr-auto">
+        <a href="index.php"><img src="assets/img/menu/logo.png" alt="" class="img-fluid"></a>
+      </div>
+
+      <nav class="nav-menu d-none d-lg-block">
+        <ul>
+          <li><a href="index.php">Home</a></li>
+          <li><a href="nosotros.php">Nosotros</a></li>
+          <li><a href="inclusion.php">Inclusión escolar</a></li>
+          <li><a href="preguntas.php">Preguntas frecuentes</a></li>
+          <li  class="active"><a href="ndestacadas.php">Información destacada</a></li>
+          <li><a href="contacto.php">Contacto</a></li>
+
+        </ul>
+      </nav><!-- .nav-menu -->
+
+    </div>
+  </header><!-- End Header -->
+  <main id="main">
+
+    <!-- ======= Breadcrumbs ======= -->
+    <section id="breadcrumbs" class="breadcrumbs">
+      <div class="container">
+
+        <ol>
+          <li><a href="index.php">ARK HO</a></li>
+          <li><a href="ndestacadas.php">Noticias Destacadas</a></li>
+        </ol>
+        <h2>REUNIÓN CON EL MUNICIPIO DE LA CIUDAD DE CÓRDOBA</h2>
+
+      </div>
+    </section><!-- End Breadcrumbs -->
+
+    <!-- ======= Blog Section ======= -->
+    <section id="blog" class="blog">
+      <div class="container">
+
+        <div class="row justify-content-center">
+
+          <div class="col-lg-8 entries">
+
+            <article class="entry entry-single ">
+
+              <div class="entry-img">
+                <img src="assets/img/ndestacadas/nd1.jpg" alt="" class="img-fluid">
+              </div>
+
+              <h2 class="entry-title">
+                <a></a>
+              </h2>
+
+
+              <div class="entry-content">
+                
+                <blockquote>
+                  <i class="icofont-quote-left quote-left"></i>
+                  <p>
+                    Creemos que las instituciones van a ser el puente a través del cual nosotros como estado vamos a poder llegar, de mejor manera, a las familias y personas con discapacidad. 
+                  </p>
+                  <i class="las la-quote-right quote-right"></i>
+                  <i class="icofont-quote-right quote-right"></i>
+                </blockquote>
+
+                <p>
+                  En el día de la fecha nos reunimos 30 organizaciones con el Municipio de la Ciudad de Córdoba, todas Instituciones con cuerpo, alma y corazón puestos en hacer conocer y respetar los derechos de las personas en situación de Discapacidad. El intendente escuchó atentamente, y luego tomó nuestras palabras, dijo que utilizaría nuestro decir “personas en situación de discapacidad”.
+                  Cuando decimos en situación (no como una condición), establecemos una mirada epistemológica, teórica, y social acerca de lo que pensamos de la realidad. Estamos convencidos que la discapacidad es una situación, en el sentido que son los distintos contextos que habitamos las personas, los que terminan convirtiendo las vivencias en situaciones discapacitantes. No le pertenece a la persona, sino a que hemos armado un mundo en donde solo pueden entrar y estar algunos, aquellos que se acercan a la arbitraria norma.
+                  
+                </p>
+
+                <h3>Nuestro horizonte</h3>
+                <p>
+                  Tenemos la certeza de andar un camino, que rompan esas situaciones, que rearmen otras, que nos permitan deconstruir miradas, y situarnos a todos en igualdad de condiciones. Celebramos gratamente el encuentro con otros compañeros de trabajo territorial, y estamos inmensamente agradecidos a la Municipalidad de Córdoba, a Martín Llaryora, a Raúl La Cava, a Leonardo Lewylle y a Pablo Giesenow por la oportunidad de dejarnos poner en palabras nuestras experiencias y trayectorias.
+
+                  <br>
+                  <br>
+                  Fundación Ark ho.
+                </p>
+                <img src="assets/img/blog-inside-post.jpg" class="img-fluid" alt="">
+
+
+              </div>
+
+              
+                <nav class="nav-menu d-none d-lg-block">
+        <ul>
+        
+          <li  class="active"><a href="ndestacadas.php">Entradas recientes >></a></li>
+          
+
+        </ul>
+      </nav><!-- .nav-menu -->
+
+
+            </article><!-- End blog entry -->
+          </div>
+        </div>
+      </div>
+    </section>
+  </main>
+
+
+  <?php include ('footer.php'); ?>
+
+  <a href="#" class="back-to-top"><i class="icofont-simple-up"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/jquery/jquery.min.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/jquery.easing/jquery.easing.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+  <script src="assets/vendor/jquery-sticky/jquery.sticky.js"></script>
+  <script src="assets/vendor/owl.carousel/owl.carousel.min.js"></script>
+  <script src="assets/vendor/waypoints/jquery.waypoints.min.js"></script>
+  <script src="assets/vendor/counterup/counterup.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/venobox/venobox.min.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
