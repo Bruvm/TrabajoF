e76d1cf641799b10850562054e018818ee919b14
meliBruve <melibruve@gmail.com>
2020-10-30 00:03:13 -0300

    Create integracion.php

---
+++ integracion.php
@@ -0,0 +1,218 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>INCLUSIÓN EDUCATIVA | Fundación Ark Ho</title>
+  <meta content="" name="descriptison">
+  <meta content="" name="keywords">
+
+  <!--link de los social media en la parte de abajo de la pagina-->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/icofont/icofont.min.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+  <link href="assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="assets/vendor/venobox/venobox.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+</head>
+
+<body>
+  <?php include ('conexion.php'); ?>
+  <header id="header">
+    <div class="container d-flex">
+
+      <div class="logo mr-auto">
+        <a href="index.php"><img src="assets/img/menu/logo.png" alt="" class="img-fluid"></a>
+      </div>
+
+      <nav class="nav-menu d-none d-lg-block">
+        <ul>
+          <li><a href="index.php">Home</a></li>
+          <li class="drop-down"><a href="#">Institucional</a>
+            <ul>
+              <li><a href="nosotros.php">Nosotros</a></li>
+              <li><a href="team.html">Estatuto</a></li>
+              <li><a href="team.html">Leyes de discapacidad </a></li>
+            </ul>
+          </li>
+          <li class="active"><a href="#">Integración Educativa</a></li>
+          <li><a href="preguntas.php">Preguntas Frecuentes</a></li>
+          <li><a href="ndestacadas.php">Información Destacada</a></li>
+          <li><a href="contacto.php">Contacto</a></li>
+        </ul>
+      </nav><!-- .nav-menu -->
+
+    </div>
+  </header><!-- End Header -->
+  
+  <main id="main">
+    <!-- ======= Breadcrumbs ======= -->
+    <section id="breadcrumbs" class="breadcrumbs">
+      <div class="container">
+
+        <ol>
+          <li><a href="index.php">ARK HO</a></li>
+          <li>Integración educativa</li>
+        </ol>
+        <h2>Integración educativa</h2>
+
+      </div>
+    </section><!-- End Breadcrumbs -->
+
+    <!-- ======= About Section ======= -->
+    <section id="about" class="about">
+      <div class="container">
+
+        <div class="row">
+          <div class="col-lg-6">
+            <img src="assets/img/about.jpg" class="img-fluid" alt="">
+          </div>
+
+          <div class="col-lg-6 pt-4 pt-lg-0 content">
+            <h2 style="color: #D68831"><b>¿Cómo realizamos el apoyo a la integración educativa?</b></h2>
+            <p class="font-italic" style="text-align: justify; font-size: 15px;">
+              Los procesos de Inclusión Educativa (antes llamados procesos de Integración Escolar) son procesos dinamicos, flexibles, sistematicos y dependen de cada situación en particular. Se trata en primer lugar de un equipo que acompaña colaborativamente a la Institución Educativa y a la familia para promover y lograr la inserción plena de las personas en situación de discapacidad. <br>
+              Un proceso se lleva a cabo mediante la selección de un DAI (Docente de Apoyo a la Inclusión) que acompaña al estudiante y a la docente de nivel en el desarrollo e implementación de los ajustes razonables y las configuraciones de apoyo necesarias en cada trayectoria educativa de los estudiantes. Cada proyecto de Inclusión Escolar es diseñado a partir del entrecruzamiento de los aportes teóricos y metodológicos de distintas disciplinas compartiendo los lineamientos  del plan de acción mundial a favor de las personas, el planeta y la prosperidad, que guiará las decisiones que adoptemos gobiernos y sociedad durante los próximos 15 años, establecidos  en la Agenda 2030
+
+            </p>
+            <!--<ul>
+              <li><i class="icofont-check-circled"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat.</li>
+              <li><i class="icofont-check-circled"></i> Duis aute irure dolor in reprehenderit in voluptate velit.</li>
+              <li><i class="icofont-check-circled"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate trideta storacalaperda mastiro dolore eu fugiat nulla pariatur.</li>
+            </ul>
+            <p>
+              Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+              velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+              culpa qui officia deserunt mollit anim id est laborum
+            </p>-->
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End About Section -->
+    <!-- ======= Testimonials Section ======= -->
+    <section id="testimonials" class="testimonials">
+      <div class="container">
+
+        <div class="row">
+
+          <div class="col-lg-6">
+            <div class="testimonial-item" >
+              <i class="icofont-check-circled"></i>
+              <span style="margin-bottom: 30px;">OBJETIVOS</span>
+              <p>
+                <i class="icofont-arrow-right" style="font-size: 14px; padding-top: 4px;text-align: justify;"></i>Promover la inclusión de niños, niñas, adolescentes, jóvenes y adultos con discapacidad o Altas Capacidades en cualquiera de las modalidades del sistema educativo argentino. 
+              </p>
+              <p>
+                <i class="icofont-arrow-right" style="font-size: 14px; padding-top: 4px;text-align: justify;"></i>Brindar a niños, niñas, adolescentes, jóvenes y adultos con discapacidad y /o Altas Capacidades los apoyos educativos y terapéuticos interdisciplinarios, acordes a las necesidades y particularidades de cada uno de ellos, a fin de lograr el máximo desarrollo de sus potencialidades.
+              </p>
+              <p>
+                <i class="icofont-arrow-right" style="font-size: 14px; padding-top: 4px;text-align: justify;"></i>Orientar a las familias y las escuelas , brindando contención e información en temas vinculados a la problemática de su hijo y a los beneficios de implementar una escuela inclusiva para todos y todas. 
+              </p>
+              <p>
+                <i class="icofont-arrow-right" style="font-size: 14px;padding-top: 4px;text-align: justify;"></i>Crear redes sociales que organicen planes de acción para una mayor inclusión de las Personas con Discapacidad en la comunidad.
+              </p>
+            </div>
+          </div>
+          <div class="col-lg-6" style="min-height : 100% ">
+            <div class="testimonial-item">
+              <i class="icofont-simple-smile"></i>
+              <span style="margin-bottom: 30px;">BENEFICIOS</span>
+              <p>
+                <i class="icofont-arrow-right" style="font-size: 14px; padding-top: 4px;text-align: justify;"></i>Desarrollo de fortalezas y dones individuales, con expectativas altas y apropiadas para cada estudiante. 
+              </p>
+              <p>
+                <i class="icofont-arrow-right" style="font-size: 14px; padding-top: 4px;text-align: justify;"></i>El trabajo sobre los objetivos individuales durante su participación en la vida escolar o universitaria con otros estudiantes hará que potencie sus capacidades.
+              </p>
+              <p>
+                <i class="icofont-arrow-right" style="font-size: 14px; padding-top: 4px;text-align: justify;"></i>
+                Involucrar a los padres en la educación académica de sus hijos y en las actividades de las escuelas de ámbito formal y no formal.
+              </p>
+              <p>
+                <i class="icofont-arrow-right" style="font-size: 14px; padding-top: 4px;text-align: justify;"></i>
+                Fomento de una cultura escolar de respeto y pertenencia.
+              </p>
+              <p>
+                <i class="icofont-arrow-right" style="font-size: 14px; padding-top: 4px;text-align: justify;"></i>
+                La educación inclusiva ofrece oportunidades para aprender y aceptar las diferencias individuales, lo que reduce el impacto de acoso e intimidación.
+              </p>
+              <p>
+                <i class="icofont-arrow-right" style="font-size: 14px; padding-top: 4px;text-align: justify;"></i>
+                Desarrollo de amistades con una amplia variedad de otros estudiantes, cada uno con sus propias necesidades y capacidades individuales.
+              </p>
+              <p>
+                <i class="icofont-arrow-right" style="font-size: 14px; padding-top: 4px;text-align: justify;"></i>
+                Aprecio de la diversidad y la inclusión en un nivel más amplio dentro de una comunidad.
+              </p>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Testimonials Section -->
+
+
+    <!-- ======= Clients Section ======= -->
+    <section id="clients" class="clients">
+      <div class="container">
+
+        <div class="section-title" style="margin-bottom: 35px;">
+          <h2>¿A quién va dirigido?</h2>
+          <p>Los procesos de Inclusión Escolar van dirigidos tanto a Personas con Discapacidad como a personas con Altas Capacidades en todas las modalidades que presenta el sistema educativo argentino: nivel inicial, primario, secundario, superior no universitario y/o universitario.  Incluyendo en dicha modalidad la Educación Hospitalaria. 
+          Además se han realizado procesos de inclusión en instituciones de educación no formal como guarderías, jardines maternales, grupos de scouts, etc.
+        </p>
+        </div>
+
+        <div class="section-title" style="margin-bottom: 35px;">
+          <h2>Tipo de Prestación</h2>
+          <p>Atención individualizada en escuela de nivel, institución terciaria y/o universitaria, consultorio y/o domicilio en forma simultánea y/o sucesiva según corresponda tanto con el estudiante como con los docentes. En caso de ser necesaria una intervención a nivel institucional, se puede realizar al igual que capacitaciones y/o asesoramiento institucional.
+        </p>
+        </div>
+
+        <div class="section-title">
+          <h2>Frecuencia de Atención</h2>
+          <p>El servicio podrá brindarse por módulos de un mínimo de 8 horas semanales o bien por hora de apoyo en caso de requerir menos de 8 horas semanales. En todos los casos, la cantidad de horas dependerá del proyecto individual e incluirá la atención individual, familiar y coordinación del equipo técnico profesional.
+        </p>
+        </div>
+        </div>-->
+
+      </div>
+    </section>
+    
+
+  </main><!-- End #main -->
+
+  <?php include ('footer.php'); ?>
+  <a href="#" class="back-to-top"><i class="icofont-simple-up"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/jquery/jquery.min.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/jquery.easing/jquery.easing.min.js"></script>
+  <script src="assets/vendor/jquery-sticky/jquery.sticky.js"></script>
+  <script src="assets/vendor/owl.carousel/owl.carousel.min.js"></script>
+  <script src="assets/vendor/waypoints/jquery.waypoints.min.js"></script>
+  <script src="assets/vendor/counterup/counterup.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/venobox/venobox.min.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+</html>
