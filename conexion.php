c500513e719659534d1c9a819f2ece948c9f20e1
nico91d <64389649+nico91d@users.noreply.github.com>
2020-10-30 20:32:08 -0300

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,12 +1,23 @@
 <?php
+
 	$dbhost = 'localhost';
+	$dbuser = 'root';
+	$dbpass = '';
+	$dbname = 'c1960018_trabajo';
+
+	$conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname) or die ('Ocurrio un error al conectarse al servidor mysql');
+	//mysqli_select_db($dbname);
+
+	error_reporting(0);
+
+	/*$dbhost = 'localhost';
 	$dbuser = 'c1960018_trabajo';
 	$dbpass = 'VEfimezo58';
 	$dbname = 'c1960018_trabajo';
 
-	$conn = mysql_connect($dbhost, $dbuser, $dbpass) or die ('Ocurrio un error al conectarse al servidor mysql');
-	mysql_select_db($dbname);
+	$conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname) or die ('Ocurrio un error al conectarse al servidor mysql');
+	//mysql_select_db($dbname);
 
-	error_reporting(0);
+	error_reporting(0);*/
 
 ?>
