911f9c4b07d681502dfa464c61769091c47e7a32
Melina Bruvera <melibruvera@hotmail.com>
2020-10-30 17:44:19 -0300

    Update validate.js

--- validate.js
+++ validate.js
@@ -1,27 +1,32 @@
 $(document).ready(function(){
 	$("#form").submit(function(){
-		var boton = $('#submit_buttom');
-		var html = "<div class='contentSpiner' style='width: 70.34px; height: 24px;'>"
-            html += "<span style='display: inline-block; width: 1.5rem; height: 1.5rem; -webkit-animation: spinner-border 1s linear infinite; animation: spinner-border 1s infinite; border-color: #fff; border-right-color: transparent;' class='spinner-border spinner-borde-sm;' role='status' aria-hidden='true'> </span>"
-            html += "</div>"
-            boton.each(function () {
-            this.prop("disabled", true);
-            this.html("hola");
-        	}
+		//var html = "<div class='contentSpiner' style='width: 70.34px; height: 24px;'>"
+            //html += "<span style='display: inline-block; width: 1.5rem; height: 1.5rem; -webkit-animation: spinner-border 1s linear infinite; animation: spinner-border 1s infinite; border-color: #fff; border-right-color: transparent;' class='spinner-border spinner-borde-sm;' role='status' aria-hidden='true'> </span>"
+            //html += "</div>"
+            //$('#submit_buttom').prop("disabled", true);
+           // $('#submit_buttom').html("hola");
 				var url = "forms/contact.php";
 				$.ajax({
 					type: "POST",
 					url: url,
 					data: $("#form").serialize(),
+					beforeSend: function(xhr, settings) {
+				        $('#submit_buttom').attr('disabled', 'disabled').prepend('<i class="fa fa-refresh fa-spin"></i> ');
+				    },
 					success: function(data){
+						$('#contentResult').addClass("enviado");
 						$("#resultado").html(data);
-						$('#form')[0].reset();
+						//$('#form')[0].reset();
 
-						$('#submit_buttom').html("Buscar");
-                        $("#submit_buttom").prop("disabled", false);
-						$('contentResult').addClass("enviado");
+						//$('#submit_buttom').html("Buscar");
+                       // $("#submit_buttom").prop("disabled", false);
+					
 					
 					},
+
+					complete: function() {
+				        $('#submit_buttom').removeAttr('disabled').find('i.fa').remove();
+				    },
 					error: function(){
 						$('#submit_buttom').html("Buscar");
                         $("#submit_buttom").prop("disabled", false);
@@ -33,4 +38,3 @@ $(document).ready(function(){
 			});
 })
 		
-			
